2f5f669aa592582fda80bfe5964176fcf22ebfd3
kalyanadapa <kalyankumaradapa821@gmail.com>
2025-01-10 10:11:58 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Auth/Navigation";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import NavHeader from "./pages/NavHeader";
 const App = () => {
   return (
     <>
       <ToastContainer />
+   <NavHeader/>
       {/* <Navigation /> */}
       <main className="">
         <Outlet />
