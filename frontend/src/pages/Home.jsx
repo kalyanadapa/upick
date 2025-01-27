442bd500ac51819e1c9ee467377bfc4ec41d4a42
kalyanadapa <kalyankumaradapa821@gmail.com>
2025-01-27 19:29:00 +0530

    updated code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,39 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Header from "../components/Header";
 import Product from "./Products/Product";
+import {  Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Swiper core styles
+import "swiper/css/navigation"; // Navigation styles
+import 'swiper/css/autoplay'; 
+import { Autoplay, Navigation, Pagination } from "swiper/modules"; // Fixed import
+const images = [
+  "https://res.cloudinary.com/dh0xxfq9y/image/upload/v1737971057/men_docgda.webp",
+  "https://res.cloudinary.com/dh0xxfq9y/image/upload/v1737971056/ttclq_pgqnux.webp",
+  "https://res.cloudinary.com/dh0xxfq9y/image/upload/v1737971056/cliq1_sdcdtq.webp",
+];
+
+const SwiperCarousel = ({ images }) => {
+  return (
+    <Swiper
+      modules={[Autoplay, Navigation, Pagination]}
+      navigation
+      pagination={{ clickable: true }}
+      loop={true}
+      className="w-full h-[60vh]"
+      autoplay={{
+        delay: 3000, // Time in milliseconds (3000ms = 3 seconds)
+        disableOnInteraction: false, // Keeps autoplay going even after user interacts
+      }}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <img src={image} alt={`Slide ${index + 1}`} className="w-full h-full object-cover" />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
 
 const Home = () => {
   const { keyword } = useParams();
@@ -44,7 +77,7 @@ const Home = () => {
           </div>
         </>
       )} */}
-      welcome to home Page
+     <SwiperCarousel images= { images}/>
     </>
   );
 };
