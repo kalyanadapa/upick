d49c778eccc6b43b2856d826eac22e355bbb35b5
kalyanadapa <kalyankumaradapa821@gmail.com>
2025-03-03 11:34:27 +0530

    configuration issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensures routes work in dev mode
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]',
+      },
+    },
+  },
 })
