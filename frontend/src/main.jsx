41b18b7e58cb29d46def8046ea0e093c5bb8c8ca
kalyanadapa <kalyankumaradapa821@gmail.com>
2025-06-12 18:55:34 +0530

    updated code with bounce

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -40,24 +40,28 @@ const Wishlist = lazy(() => import("./pages/User/Wishlist.jsx"));
 
 //Animated Framer Motion Loader
 const AnimatedLoader = () => {
-  const bounce = {
-    y: {
-      duration: 0.5,
-      repeat: Infinity,
-      repeatType: "reverse",
-      ease: "easeInOut",
-    },
-  };
+
 
   return (
     <div className="flex justify-center items-center h-64">
       <motion.span
-        className="text-3xl font-extrabold bg-gradient-to-r from-pink-700 via-orange-400 to-pink-700 text-transparent bg-clip-text"
-        animate={{ backgroundPosition: ["0% 50%", "100% 50%"] }}
+        className="text-4xl font-extrabold bg-gradient-to-r from-pink-700 via-orange-400 to-pink-700 text-transparent bg-clip-text"
+        animate={{
+          y: ["0%", "-10%", "0%"], // bounce effect
+          backgroundPosition: ["0% 50%", "100% 50%"], // shimmer effect
+        }}
         transition={{
-          duration: 2,
-          repeat: Infinity,
-          ease: "linear",
+          y: {
+            duration: 0.6,
+            repeat: Infinity,
+            repeatType: "loop",
+            ease: "easeInOut",
+          },
+          backgroundPosition: {
+            duration: 2,
+            repeat: Infinity,
+            ease: "linear",
+          },
         }}
         style={{
           backgroundSize: "200% 200%",
