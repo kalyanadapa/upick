cf2a771d0e09ad4b4994941940688cf4a3879515
kalyanadapa <kalyankumaradapa821@gmail.com>
2025-06-13 19:52:32 +0530

    reverted to old code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,52 +7,52 @@
 // })
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       output: {
-//         entryFileNames: 'assets/[name].[hash].js',
-//         chunkFileNames: 'assets/[name].[hash].js',
-//         assetFileNames: 'assets/[name].[hash][extname]',
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]',
+      },
+    },
+    
+  },
+   server: {
+    historyApiFallback: true,
+  }
+})
+/* eslint-env node */
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd());
+
+//   return {
+//     plugins: [react()],
+//     build: {
+//       rollupOptions: {
+//         output: {
+//           entryFileNames: 'assets/[name].[hash].js',
+//           chunkFileNames: 'assets/[name].[hash].js',
+//           assetFileNames: 'assets/[name].[hash][extname]',
+//         },
 //       },
 //     },
-    
-//   },
-//    server: {
-//     historyApiFallback: true,
-//   }
-// })
-/* eslint-env node */
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    build: {
-      rollupOptions: {
-        output: {
-          entryFileNames: 'assets/[name].[hash].js',
-          chunkFileNames: 'assets/[name].[hash].js',
-          assetFileNames: 'assets/[name].[hash][extname]',
-        },
-      },
-    },
-    server: {
-      historyApiFallback: true,
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-  };
-});
+//     server: {
+//       historyApiFallback: true,
+//       proxy: {
+//         '/api': {
+//           target: env.VITE_API_URL,
+//           changeOrigin: true,
+//           secure: false,
+//         },
+//       },
+//     },
+//   };
+// });
